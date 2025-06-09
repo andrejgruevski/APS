332ac0522b41234f45836d3dff268a53c2718d5c
andrejgruevski <andrejgruevski1@gmail.com>
2025-06-09 20:03:09 +0200

    initial commit

---
+++ src/DLL/zad11/Main.java
@@ -0,0 +1,242 @@
+package DLL.zad11;
+
+import java.util.Scanner;
+
+class DLLNode<E> {
+    protected E element;
+    protected DLLNode<E> pred, succ;
+    public DLLNode(E elem, DLLNode<E> pred, DLLNode<E> succ) {
+        this.element = elem;
+        this.pred = pred;
+        this.succ = succ;
+    }
+
+    @Override
+    public String toString() {
+        return element.toString();
+    }
+}
+
+class DLL<E> {
+    private DLLNode<E> first, last;
+
+    public DLL() {
+        // Construct an empty SLL
+        this.first = null;
+        this.last = null;
+    }
+
+    public void insertFirst(E o) {
+        DLLNode<E> ins = new DLLNode<E>(o, null, first);
+        if (first == null)
+            last = ins;
+        else
+            first.pred = ins;
+        first = ins;
+    }
+
+    public void insertLast(E o) {
+        if (first == null)
+            insertFirst(o);
+        else {
+            DLLNode<E> ins = new DLLNode<E>(o, last, null);
+            last.succ = ins;
+            last = ins;
+        }
+    }
+
+    public void insertAfter(E o, DLLNode<E> after) {
+        if (after == last) {
+            insertLast(o);
+            return;
+        }
+        DLLNode<E> ins = new DLLNode<E>(o, after, after.succ);
+        after.succ.pred = ins;
+        after.succ = ins;
+    }
+
+    public void insertBefore(E o, DLLNode<E> before) {
+        if (before == first) {
+            insertFirst(o);
+            return;
+        }
+        DLLNode<E> ins = new DLLNode<E>(o, before.pred, before);
+        before.pred.succ = ins;
+        before.pred = ins;
+    }
+
+    public E deleteFirst() {
+        if (first != null) {
+            DLLNode<E> tmp = first;
+            first = first.succ;
+            if (first != null) first.pred = null;
+            if (first == null)
+                last = null;
+            return tmp.element;
+        } else
+            return null;
+    }
+
+    public E deleteLast() {
+        if (first != null) {
+            if (first.succ == null)
+                return deleteFirst();
+            else {
+                DLLNode<E> tmp = last;
+                last = last.pred;
+                last.succ = null;
+                return tmp.element;
+            }
+        } else
+            return null;
+    }
+
+    public E delete(DLLNode<E> node) {
+        if (node == first) {
+            return deleteFirst();
+        }
+        if (node == last) {
+            return deleteLast();
+        }
+        node.pred.succ = node.succ;
+        node.succ.pred = node.pred;
+        return node.element;
+
+    }
+
+    public DLLNode<E> find(E o) {
+        if (first != null) {
+            DLLNode<E> tmp = first;
+            while (!tmp.element.equals(o) && tmp.succ != null)
+                tmp = tmp.succ;
+            if (tmp.element.equals(o)) {
+                return tmp;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+            }
+        } else {
+            System.out.println("Listata e prazna");
+        }
+        return null;
+    }
+
+    public void deleteList() {
+        first = null;
+        last = null;
+    }
+
+    public int getSize() {
+        int listSize = 0;
+        DLLNode<E> tmp = first;
+        while(tmp != null) {
+            listSize++;
+            tmp = tmp.succ;
+        }
+        return listSize;
+    }
+
+    @Override
+    public String toString() {
+        String ret = new String();
+        if (first != null) {
+            DLLNode<E> tmp = first;
+            ret += tmp.toString();
+            while (tmp.succ != null) {
+                tmp = tmp.succ;
+                ret += "<->" + tmp.toString();
+            }
+        } else
+            ret = "Prazna lista!!!";
+        return ret;
+    }
+
+    public String toStringR() {
+        String ret = new String();
+        if (last != null) {
+            DLLNode<E> tmp = last;
+            ret += tmp.toString();
+            while (tmp.pred != null) {
+                tmp = tmp.pred;
+                ret += "<->" + tmp.toString();
+            }
+        } else
+            ret = "Prazna lista!!!";
+        return ret;
+    }
+
+    public DLLNode<E> getFirst() {
+        return first;
+    }
+
+    public DLLNode<E> getLast() {
+
+        return last;
+    }
+
+    public void mirror() {
+
+        DLLNode<E> tmp = null;
+        DLLNode<E> current = first;
+        last = first;
+        while(current!=null) {
+            tmp = current.pred;
+            current.pred = current.succ;
+            current.succ = tmp;
+            current = current.pred;
+        }
+
+        if(tmp!=null && tmp.pred!=null) {
+            first=tmp.pred;
+        }
+    }
+}
+//Дадена е двострано поврзана листа од цели броеви. Дополнително, дадени се и уште еден цел број M и еден природен број k. Треба да се најде првото појавување на M во листата и тој број да се помести k места на десно.
+//
+//Влез: Во првиот ред од влезот е даден бројот на елементи во листата - N, па во следните следниот ред самите елементи одделени со празно место. На крај, во последните два реда дадени се целиот број M и природниот број k.
+//
+//Излез: На излез треба да се испечати листата пред и после промената.
+public class Main {
+    public static void function(DLL<Integer> lista, int m, int k){
+        DLLNode<Integer> iterator = lista.getFirst();
+        DLLNode<Integer> mElement = null;
+        while (iterator != null) {
+            if (iterator.element.equals(m)){
+                mElement = iterator;
+                break;
+            }
+            iterator = iterator.succ;
+        }
+        if (mElement == null) {
+            System.out.println("Elementot ne postoi vo listata");
+          return;
+        }
+
+        for (int i =0; i<k; i++){
+            if (mElement == lista.getLast()){
+                lista.deleteLast();
+                lista.insertFirst(m);
+                mElement = lista.getFirst();
+            }else{
+                DLLNode<Integer> tmp = mElement.succ;
+                lista.delete(mElement);
+                lista.insertAfter(m,tmp);
+                mElement = tmp.succ;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        DLL<Integer> lista = new DLL<>();
+
+        for (int i =0; i<n; i++){
+            lista.insertLast(sc.nextInt());
+        }
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        System.out.println(lista);
+        function(lista,m,k);
+        System.out.println(lista);
+    }
+}
