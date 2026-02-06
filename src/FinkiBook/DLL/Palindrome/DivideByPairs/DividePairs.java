fbfdc84d689ca6de8e44c712f9b5ad5b3b8030a1
Andrej Gruevski <andrejgruevski1@gmail.com>
2026-02-06 15:31:39 +0100

    Divide Pairs DLL TASK

---
+++ src/FinkiBook/DLL/Palindrome/DivideByPairs/DividePairs.java
@@ -0,0 +1,245 @@
+package FinkiBook.DLL.Palindrome.DivideByPairs;
+
+import java.util.Scanner;
+
+class DLLNode<E> {
+    protected E element;
+    protected DLLNode<E> pred, succ;
+    public DLLNode(E elem, DLLNode<E> pred, DLLNode<E> succ) {
+        this.element = elem;
+        this.pred = pred;
+        this.succ = succ;
+    }
+
+    @Override
+    public String toString() {
+        return element.toString();
+    }
+}
+
+
+
+class DLL<E> {
+    private DLLNode<E> first, last;
+
+    public DLL() {
+        // Construct an empty SLL
+        this.first = null;
+        this.last = null;
+    }
+
+    public void insertFirst(E o) {
+        DLLNode<E> ins = new DLLNode<E>(o, null, first);
+        if (first == null)
+            last = ins;
+        else
+            first.pred = ins;
+        first = ins;
+    }
+
+    public void insertLast(E o) {
+        if (first == null)
+            insertFirst(o);
+        else {
+            DLLNode<E> ins = new DLLNode<E>(o, last, null);
+            last.succ = ins;
+            last = ins;
+        }
+    }
+
+    public void insertAfter(E o, DLLNode<E> after) {
+        if (after == last) {
+            insertLast(o);
+            return;
+        }
+        DLLNode<E> ins = new DLLNode<E>(o, after, after.succ);
+        after.succ.pred = ins;
+        after.succ = ins;
+    }
+
+    public void insertBefore(E o, DLLNode<E> before) {
+        if (before == first) {
+            insertFirst(o);
+            return;
+        }
+        DLLNode<E> ins = new DLLNode<E>(o, before.pred, before);
+        before.pred.succ = ins;
+        before.pred = ins;
+    }
+
+    public E deleteFirst() {
+        if (first != null) {
+            DLLNode<E> tmp = first;
+            first = first.succ;
+            if (first != null) first.pred = null;
+            if (first == null)
+                last = null;
+            return tmp.element;
+        } else
+            return null;
+    }
+
+    public E deleteLast() {
+        if (first != null) {
+            if (first.succ == null)
+                return deleteFirst();
+            else {
+                DLLNode<E> tmp = last;
+                last = last.pred;
+                last.succ = null;
+                return tmp.element;
+            }
+        } else
+            return null;
+    }
+
+    public E delete(DLLNode<E> node) {
+        if (node == first) {
+            return deleteFirst();
+        }
+        if (node == last) {
+            return deleteLast();
+        }
+        node.pred.succ = node.succ;
+        node.succ.pred = node.pred;
+        return node.element;
+
+    }
+
+    public DLLNode<E> find(E o) {
+        if (first != null) {
+            DLLNode<E> tmp = first;
+            while (!tmp.element.equals(o) && tmp.succ != null)
+                tmp = tmp.succ;
+            if (tmp.element.equals(o)) {
+                return tmp;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+            }
+        } else {
+            System.out.println("Listata e prazna");
+        }
+        return null;
+    }
+
+    public void deleteList() {
+        first = null;
+        last = null;
+    }
+
+    public int getSize() {
+        int listSize = 0;
+        DLLNode<E> tmp = first;
+        while(tmp != null) {
+            listSize++;
+            tmp = tmp.succ;
+        }
+        return listSize;
+    }
+
+    @Override
+    public String toString() {
+        String ret = new String();
+        if (first != null) {
+            DLLNode<E> tmp = first;
+            ret += tmp.toString();
+            while (tmp.succ != null) {
+                tmp = tmp.succ;
+                ret += "<->" + tmp.toString();
+            }
+        } else
+            ret = "Prazna lista!!!";
+        return ret;
+    }
+
+    public String toStringR() {
+        String ret = new String();
+        if (last != null) {
+            DLLNode<E> tmp = last;
+            ret += tmp.toString();
+            while (tmp.pred != null) {
+                tmp = tmp.pred;
+                ret += "<->" + tmp.toString();
+            }
+        } else
+            ret = "Prazna lista!!!";
+        return ret;
+    }
+
+    public DLLNode<E> getFirst() {
+        return first;
+    }
+
+    public DLLNode<E> getLast() {
+
+        return last;
+    }
+
+    public void mirror() {
+
+        DLLNode<E> tmp = null;
+        DLLNode<E> current = first;
+        last = first;
+        while(current!=null) {
+            tmp = current.pred;
+            current.pred = current.succ;
+            current.succ = tmp;
+            current = current.pred;
+        }
+
+        if(tmp!=null && tmp.pred!=null) {
+            first=tmp.pred;
+        }
+    }
+}
+//Дадена е двострано поврзана листа чии што jазли содржат по еден природен
+//броj. Листата треба да се подели на две резултантни листи, т.ш. во првата резултанта листа ´ке бидат бидат сместени jазли од влезната листа кои содржат парни
+//броеви, а во втората – непарните. Jазлите во резултантните листи се додаваат
+//наизменично почнуваj´ки од почетокот и краjот на влезната листа (т.е. прво се
+//разгледува првиот елемент од листата и се додава во соодветната резултантна
+//        листа, па последниот, па вториот итн...)
+public class DividePairs {
+    public static void pairs(DLL<Integer> list){
+        DLLNode<Integer> first = list.getFirst();
+        DLLNode<Integer> last = list.getLast();
+
+        DLL<Integer> even = new DLL<>();
+        DLL<Integer> odd = new DLL<>();
+        // first != last for odd N in list, last.succ != first for even N in list
+        while (first != last &&  last.succ != first) {
+            if (first.element%2 == 0){
+                even.insertLast(first.element);
+            }else{
+                odd.insertLast(first.element);
+            }
+            if (last.element%2 == 0){
+                even.insertLast(last.element);
+            }else {
+                odd.insertLast(last.element);
+            }
+            first = first.succ;
+            last = last.pred;
+        }
+
+        // we put the last number here
+        if (first == last) {
+            if (first.element%2 == 0){
+                even.insertLast(first.element);
+            }else {
+                odd.insertLast(first.element);
+            }
+        }
+        System.out.println("Even List: "+even);
+        System.out.println("Odd List: "+odd);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        DLL<Integer> list = new DLL<>();
+        for (int i = 0; i < n; i++) {
+            list.insertLast(sc.nextInt());
+        }
+        pairs(list);
+
+    }
+}
